0a7ad8c05fa51845d2ec14d59e25f0309bcbd1fa
Christopher Matthew <christopher.s.matthew@gmail.com>
2024-12-01 01:49:05 +0000

    Rebuild

---
+++ assets/js/script-rb.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Connected to script.js");
+
+    // Get references to DOM elements
+  const paddle = document.getElementById("paddle");
+
+
+   let paddlePosition = 250; // Initial position of the paddle
+}
